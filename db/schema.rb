e512754bc0d92c9bdf429acb02464c2a5920ff00
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 16:58:19 +0000

    Created seeds file for sample data and modified scaffolding for Blogs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615170918) do
+ActiveRecord::Schema.define(version: 20170619161404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
