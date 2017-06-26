9ef16e45a87172cc21dab758245595dcd436696c
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 14:58:17 +0000

    Implemented custom scopes for portfolio items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created."
+
 10.times do |blog|
   Blog.create!(
     title: "My Rails #{blog}",
-    body: "no."
+    body: "no.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created."
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title #{portfolio_item}",
       subtitle: "Ruby on Rails",
+      body: "yes.",
+      main_image: "http://via.placeholder.com/700x400",
+      thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+      title: "Portfolio title #{portfolio_item}",
+      subtitle: "Angular",
       body: "yes.",
       main_image: "http://via.placeholder.com/700x400",
       thumb_image: "http://via.placeholder.com/350x200"
