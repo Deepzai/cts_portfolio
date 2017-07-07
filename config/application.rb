5ba1be7ba95a27dd6153d2ace1fda6d65dec1cad
Jordan Hudgens <jordan@devcamp.com>
2017-07-07 21:48:39 +0000

    Refactored strong params for portfolio items.

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,5 @@ Bundler.require(*Rails.groups)
 
 module CTSRails
   class Application < Rails::Application
-   # config.generators do |g|
-     # g.orm             :active_record
-    #  g.template_engine :erb
-     # g.test_framework  :test_unit, fixture: false
-     # g.stylesheets     false
-     # g.javascripts     true
-   # end
   end
 end
