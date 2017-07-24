423ae97db087d456ea4fc1cfc6a21ae5cfa914fd
Jason Liu <jasonningliu@gmail.com>
2017-07-24 20:42:47 +0000

    Updated form styles for portfolios.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,7 @@ class PortfoliosController < ApplicationController
   
   def index 
     @portfolio_items = Portfolio.all
+    
   end
   
   def angular
@@ -40,12 +41,13 @@ class PortfoliosController < ApplicationController
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Your portfolio was successfully updated.' }
       else
-        format.html { render :edit }
+        format.html { redirect_to portfolios_path }
       end
     end
   end
   
   def show 
+    
   end
   
   def destroy
